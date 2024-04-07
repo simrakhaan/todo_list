3eb7d9ce9f82da0a154bfdbfd4a6cfe029ea4245
simrakhaan <159695507+simrakhaan@users.noreply.github.com>
2024-04-08 01:38:42 +0500

    my first todo list

---
+++ main.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+import inquirer from "inquirer"
+let todos = [];
+let condition  = true;
+while (condition)
+{
+let todoQuestion = await inquirer.prompt(
+  [
+  {
+name:"firstQuestion" ,
+type:"input",
+message:"what you want to add in your todos?",
+  },
+  {
+    name: "secondQuestion",
+    type:"confirm",
+    message:"do you want to add more ?",
+    default: "true",
+  }
+  ]
+    );
+    todos.push(todoQuestion.firstQuestion);
+    console.log(todos);
+    condition = todoQuestion . secondQuestion}
